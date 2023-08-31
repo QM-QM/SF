1efc6747e0ffb523502c7fc9c510ae6c20f9cffe
QM-QM <70558015+QM-QM@users.noreply.github.com>
2023-08-31 02:40:30 +0200

    Add files via upload

--- battleships.py
+++ battleships.py
@@ -1,14 +1,14 @@
 import random
 
 
-# Error Message
+# Error message
 class BoardOutException(Exception):
     def __init__(self, msg="You shot outside of play zone!"):
         self.msg = msg
         Exception.__init__(self, self.msg)
 
 
-# Position
+# Position of Ship's X & Y
 class Dot:
     def __init__(self, x, y):
         self.x = x
@@ -26,27 +26,28 @@ class Ship:
         self.length = length
         self.tip = tip
         self.direction = direction
+        self.lives = length
 
     def dots(self):
         ship_dots = []
-        if self.direction == "vertical":
+        if self.direction == "Vertical":
             # Vertical Axis
             for i in range(self.length):
-                ship_dots.append((self.tip.x, self.tip.y + i))
+                ship_dots.append(Dot(self.tip.x, self.tip.y + i))
         else:  # Horizontal Axis
             for i in range(self.length):
-                ship_dots.append((self.tip.x + i, self.tip.y))
+                ship_dots.append(Dot(self.tip.x + i, self.tip.y))
         return ship_dots
 
 
+# Field
 class Board:
     def __init__(self, size, hid=True):
         self.alive_ships = 0
         self.hid = hid
         self.size = size
         self.ships = []
-        self.ships_alive = 0
-        self.grid = [[" "] * 5 for _ in range(size)]
+        self.grid = [[' '] * size for _ in range(size)]
 
     def add_ships(self, ship):
         for dot in ship.dots():
@@ -54,46 +55,57 @@ class Board:
                 raise ValueError("Unable to place a ship here.")
         for dot in ship.dots():
             self.grid[dot.x][dot.y] = 'O'
-        self.ships.append(ship)
-        self.ships_alive += 1
+            self.ships.append(ship)
+        self.alive_ships += 1  # Внесены изменеине
 
+    #
     def contour(self, ship):
         for dot in ship.dots():
             for x in range(max(0, self.size - 1), min(self.size, dot.x + 2)):
-                for y in range(max(0, self.size - 1), min(self.size, dot.x + 2)):
-                    if self.grid[x][y] == " ":
+                for y in range(max(0, self.size - 1), min(self.size, dot.y + 2)):
+                    if self.grid[x][y] == ' ':
                         self.grid[x][y] = "X"
 
-    def out(self, dot):
-        return not (0 <= dot.x < self.size and 0 <= dot.y < self.size)
+    # Checks if Ships are hidden or not
+    def is_hidden(self):
+        self.hid = True
+
+    def is_not_hidden(self):
+        self.hid = False
+
+    # Display a playing field
+    def display(self):
+        print("  | " + "   ".join(str(i) for i in range(self.size)))
+        print("--+" + "----" * 1 * self.size)
+        for i, row in enumerate(self.grid):
+            row_str = " | ".join(cell if not self.hid or cell != 'O' else ' ' for cell in row)
+            print(f"{i} | {row_str}")
 
     def shot(self, dot):
         if self.out(dot):
             raise ValueError("Point out of the board.")
-        if self.grid[dot.x][dot.y] != ' ':
+        if not self.grid[dot.x][dot.y] != 'T':
             raise ValueError("You already shot there.")
-        self.grid[dot.x][dot.y] = '.'
+
+        hit_ship = None
         for ship in self.ships:
             if dot in ship.dots():
                 ship.lives -= 1
                 if ship.lives == 0:
                     self.alive_ships -= 1
                     self.contour(ship)
-                return True
-        return False
+                hit_ship = ship
+                break
 
-    def display(self):
-        print("  | " + " ".join(str(i) for i in range(self.size)))
-        print("--+" + "-" * 2 * self.size)
-        for i, row in enumerate(self.grid):
-            row_str = " | ".join(cell if not self.hid or cell != 'O' else ' ' for cell in row)
-            print(f"{i} | {row_str}")
+        if hit_ship:
+            self.grid[dot.x][dot.y] = 'X'
+            return True
+        else:
+            self.grid[dot.x][dot.y] = 'T'
+            return False
 
-    def is_hidden(self):
-        self.hid = True
-
-    def is_not_hidden(self):
-        self.hid = False
+    def out(self, dot):
+        return not (0 <= dot.x < self.size and 0 <= dot.y < self.size)
 
 
 class Player:
@@ -101,7 +113,8 @@ class Player:
         self.own_board = own_board
         self.enemy_board = enemy_board
 
-    def ask(self):
+    @staticmethod
+    def ask():
         raise NotImplementedError("The ask method should be implemented by subclasses")
 
     def move(self):
@@ -115,7 +128,8 @@ class Player:
 
 
 class User(Player):
-    def ask(self):
+    @staticmethod
+    def ask():
         while True:
             try:
                 x = int(input("Enter X coordinate: "))
@@ -133,31 +147,26 @@ class AI(Player):
         return Dot(x, y)
 
 
-def greet():
-    print("Welcome to Battleship!")
-    print("Try to sink all enemy ships.")
-
-
 class Game:
     def __init__(self):
-        self.user_board = Board(10)
-        self.ai_board = Board(10)
+        self.user_board = Board(10, hid=False)
+        self.ai_board = Board(10, hid=True)  # True | Скрыть, False | Показать место положение кораблей у противника
         self.user = User(self.user_board, self.ai_board)
         self.ai = AI(self.ai_board, self.user_board)
 
     @staticmethod
     def random_board(board):
-        ship_lengths = [3, 2, 2, 2, 1, 1, 1, 1]
+        ship_lengths = [3, 2, 2, 1, 1, 1, 1]
 
         for length in ship_lengths:
             while True:
                 direction = random.choice(['Vertical', 'Horizontal'])
                 x = random.randint(0, board.size - 1)
                 y = random.randint(0, board.size - 1)
-                bow = Dot(x, y)
+                tip = Dot(x, y)
 
                 try:
-                    ship = Ship(length, bow, direction)
+                    ship = Ship(length, tip, direction)
                     board.add_ships(ship)
                     break
                 except ValueError:
@@ -165,25 +174,38 @@ class Game:
 
     def loop(self):
         while self.user_board.alive_ships > 0 and self.ai_board.alive_ships > 0:
+            # Display players field
             self.user_board.display()
             self.ai_board.display()
+
+            # Making so player and Ai can do their move. (And for some reason also breaking after losing or winning)
             user_repeat = self.user.move()
             ai_repeat = self.ai.move()
-
-            if not (user_repeat or ai_repeat):
+            if not user_repeat:
+                self.user_board.display()
+                self.ai_board.display()
+                print("Congratulations! You won.")
+                break
+            if not ai_repeat:
+                self.user_board.display()
+                self.ai_board.display()
+                print("Sorry, you lost.")
                 break
 
-        if self.user_board.alive_ships == 0:
-            print("Sorry, you lost.")
-        else:
-            print("Congratulations! You won.")
+            # Making sure if player or Ai have 0 Ships on Field
+            if self.user_board.alive_ships == 0:
+                break
 
+            elif self.ai_board.alive_ships == 0:
+                break
+
+    # Start Up
     def start(self):
-        greet()
         self.random_board(self.user_board)
         self.random_board(self.ai_board)
         self.loop()
 
 
+print("Welcome to Battleship!")
 game = Game()
 game.start()
